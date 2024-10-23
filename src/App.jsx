3951e8b4aa4b56f11b6b8988ef068f453062325b
Muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2024-10-23 14:07:36 +0500

    practice

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+
+
+
+
+const App = () => {
+  return(
+    <>
+    <div> 
+       <div>
+         <img src="vite.svg" alt="vite.svg" width="25%" />
+       </div>
+        <h2>Name: Queen Tears Of Korean</h2>
+        <h3>Rating: 8.2</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor molestiae perspiciatis rem odit eligendi et, est unde quo error ad quia. Minima, deserunt delectus. Nostrum doloremque magni perferendis sint eos vitae quaerat minus illum nam libero, itaque expedita officiis nihil laborum, praesentium suscipit rem. Expedita porro esse impedit ea. Tenetur.</p>
+        </div>
+        <div> 
+       <div>
+         <img src="vite.svg" alt="vite.svg" width="25%" />
+       </div>
+        <h2>Name: Queen Tears Of Korean</h2>
+        <h3>Rating: 8.2</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor molestiae perspiciatis rem odit eligendi et, est unde quo error ad quia. Minima, deserunt delectus. Nostrum doloremque magni perferendis sint eos vitae quaerat minus illum nam libero, itaque expedita officiis nihil laborum, praesentium suscipit rem. Expedita porro esse impedit ea. Tenetur.</p>
+        </div>
+        <div> 
+       <div>
+         <img src="vite.svg" alt="vite.svg" width="25%" />
+       </div>
+        <h2>Name: Queen Tears Of Korean</h2>
+        <h3>Rating: 8.2</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor molestiae perspiciatis rem odit eligendi et, est unde quo error ad quia. Minima, deserunt delectus. Nostrum doloremque magni perferendis sint eos vitae quaerat minus illum nam libero, itaque expedita officiis nihil laborum, praesentium suscipit rem. Expedita porro esse impedit ea. Tenetur.</p>
+        </div>
+        <div> 
+       <div>
+         <img src="vite.svg" alt="vite.svg" width="25%" />
+       </div>
+        <h2>Name: Queen Tears Of Korean</h2>
+        <h3>Rating: 8.2</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor molestiae perspiciatis rem odit eligendi et, est unde quo error ad quia. Minima, deserunt delectus. Nostrum doloremque magni perferendis sint eos vitae quaerat minus illum nam libero, itaque expedita officiis nihil laborum, praesentium suscipit rem. Expedita porro esse impedit ea. Tenetur.</p>
+        </div>
+        <div> 
+       <div>
+         <img src="vite.svg" alt="vite.svg" width="25%" />
+       </div>
+        <h2>Name: Queen Tears Of Korean</h2>
+        <h3>Rating: 8.2</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor molestiae perspiciatis rem odit eligendi et, est unde quo error ad quia. Minima, deserunt delectus. Nostrum doloremque magni perferendis sint eos vitae quaerat minus illum nam libero, itaque expedita officiis nihil laborum, praesentium suscipit rem. Expedita porro esse impedit ea. Tenetur.</p>
+        </div>
+      
+        </>
+  )
+}
+
+export default App;
