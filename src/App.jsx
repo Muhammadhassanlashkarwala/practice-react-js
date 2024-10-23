6b68e74fe2538d98538ae183441053ba9c77a561
Muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2024-10-23 14:15:07 +0500

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 
-
-
-
+// Now i simple work in react create a function then return in 5 divs. 
 const App = () => {
   return(
     <>
-    <div> 
+    {/* <div> 
        <div>
          <img src="vite.svg" alt="vite.svg" width="25%" />
        </div>
@@ -45,9 +43,35 @@ const App = () => {
         <h3>Rating: 8.2</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor molestiae perspiciatis rem odit eligendi et, est unde quo error ad quia. Minima, deserunt delectus. Nostrum doloremque magni perferendis sint eos vitae quaerat minus illum nam libero, itaque expedita officiis nihil laborum, praesentium suscipit rem. Expedita porro esse impedit ea. Tenetur.</p>
         </div>
+
+i commit all divs and call the component only */}
+  
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
       
         </>
   )
 }
+
+// Now work easy so i use components in react 
+// first i create a component, and made a component in Pascal case
+
+const NetflixSeries = () => {
+  return(
+    <div> 
+  <div>
+    <img src="vite.svg" alt="vite.svg" width="25%" />
+  </div>
+   <h2>Name: Queen Tears Of Korean</h2>
+   <h3>Rating: 8.2</h3>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor molestiae perspiciatis rem odit eligendi et, est unde quo error ad quia. Minima, deserunt delectus. Nostrum doloremque magni perferendis sint eos vitae quaerat minus illum nam libero, itaque expedita officiis nihil laborum, praesentium suscipit rem. Expedita porro esse impedit ea. Tenetur.</p>
+   </div>
+  );
+};
+
+
 
 export default App;
