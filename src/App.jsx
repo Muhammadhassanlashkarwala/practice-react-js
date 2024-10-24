3017b6af116a83b20ec3de169b4af7e54af66a87
Muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2024-10-24 14:35:04 +0500

    push

--- src/App.jsx
+++ src/App.jsx
@@ -59,19 +59,42 @@ i commit all divs and call the component only */}
 // Now work easy so i use components in react 
 // first i create a component, and made a component in Pascal case
 
+// const NetflixSeries = () => {
+//   return(
+//     <div> 
+//   <div>
+//     <img src="vite.svg" alt="vite.svg" width="25%" />
+//   </div>
+//    <h2>Name: Queen Tears Of Korean</h2>
+//    <h3>Rating: 8.2</h3>
+//    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor molestiae perspiciatis rem odit eligendi et, est unde quo error ad quia. Minima, deserunt delectus. Nostrum doloremque magni perferendis sint eos vitae quaerat minus illum nam libero, itaque expedita officiis nihil laborum, praesentium suscipit rem. Expedita porro esse impedit ea. Tenetur.</p>
+//    </div>
+
+ // i commit this but scroll down same work with the dynamics values   
+//   );
+// };
+
+
+// Now use dynamic values in react.
+// how to use dynamic values in react ? so it's easy i use javascript in react so i also used this curly braces only {} i use curly braces so i enter javascript world.
+
 const NetflixSeries = () => {
+  const name = "Queen Tears Of Korean" ;
+  const rating = 8.2 ;
+  const description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor molestiae perspiciatis rem odit eligendi et, est unde quo error ad quia. Minima, deserunt delectus. Nostrum doloremque magni perferendis sint eos vitae quaerat minus illum nam libero, itaque expedita officiis nihil laborum, praesentium suscipit rem. Expedita porro esse impedit ea. Tenetur."
+
   return(
     <div> 
   <div>
     <img src="vite.svg" alt="vite.svg" width="25%" />
   </div>
-   <h2>Name: Queen Tears Of Korean</h2>
-   <h3>Rating: 8.2</h3>
-   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor molestiae perspiciatis rem odit eligendi et, est unde quo error ad quia. Minima, deserunt delectus. Nostrum doloremque magni perferendis sint eos vitae quaerat minus illum nam libero, itaque expedita officiis nihil laborum, praesentium suscipit rem. Expedita porro esse impedit ea. Tenetur.</p>
+   <h2>Name: {name}</h2>
+   <h3>Rating: {rating}</h3>
+   <p> <b>Summary</b> : {description} </p>
    </div>
+
+   
   );
 };
-
-
 
 export default App;
