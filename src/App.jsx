31ff361fe9d50f3f0dae9f2ebf3d10c1af92edb4
Muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2024-10-27 23:50:00 +0500

    Added Practice.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 
-
 // Now i simple work in react create a function then return in 5 divs. 
 const App = () => {
   return(
@@ -15,12 +14,14 @@ const App = () => {
         
           Now i comment all divs and simply call a component only...
                   Component your work Easy */}
+{/* 
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries /> */}
 
-      <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />
+      <AnimeArt />
    
         </>
   )
@@ -48,31 +49,55 @@ const App = () => {
 // Now use dynamic values in react.
 // how to use dynamic values in react ? so it's easy i use javascript in react so i also used this curly braces only {} i use curly braces so i enter javascript world.
 
-const NetflixSeries = () => {
-  const name = "Queen Tears Of Korean" ;
-  const rating = 8.2 ;
-  const description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor molestiae perspiciatis rem odit eligendi et, est unde quo error ad quia. Minima, deserunt delectus. Nostrum doloremque magni perferendis sint eos vitae quaerat minus illum nam libero, itaque expedita officiis nihil laborum, praesentium suscipit rem. Expedita porro esse impedit ea. Tenetur."
+// const NetflixSeries = () => {
+//   const name = "Queen Tears Of Korean" ;
+//   const rating = 8.2 ;
+//   const description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor molestiae perspiciatis rem odit eligendi et, est unde quo error ad quia. Minima, deserunt delectus. Nostrum doloremque magni perferendis sint eos vitae quaerat minus illum nam libero, itaque expedita officiis nihil laborum, praesentium suscipit rem. Expedita porro esse impedit ea. Tenetur."
 
-  const returnGenre = () => {
-     const genre = "Romcop And Honey";
-    return genre;
-  };
+//   const returnGenre = () => {
+//      const genre = "Romcop And Honey";
+//     return genre;
+//   };
 
-  return(
-    <div> 
-  <div>
-    <img src="vite.svg" alt="vite.svg" width="11%" />
-  </div>
-   <h2>Name: {name}</h2>
-   <h3>Rating: {rating}</h3>
-   <p> <b>Summary</b> : {description} </p>
-   <p><b>Genre: </b>{ returnGenre() }</p>
-    <hr />
-   </div>
+//   return(
+//     <div> 
+//   <div>
+//     <img src="vite.svg" alt="vite.svg" width="11%" />
+//   </div>
+//    <h2>Name: {name}</h2>
+//    <h3>Rating: {rating}</h3>
+//    <p> <b>Summary</b> : {description} </p>
+//    <p><b>Genre: </b>{ returnGenre() }</p>
+//     <hr />
+//    </div>
 
    
-  );
-};
+//   );
+// };
 
+
+//  Now Today Learn How To Use 
+//  Conditional Values In JSX
+
+
+const AnimeArt = () => {
+  const name = 'DBZ🥰';
+  const image =  <img src="anime.jpeg" alt="" width="30%" />
+  const description  = 'Lorem ist piece gist.'
+  const age = 15 
+  
+
+    return(                   
+        <>                                                 
+      <h1>Name : {name}</h1>                               
+      <h1>Description : {description}</h1>                               
+      <div>
+        {image}
+      </div>
+      <button>{age >= 18 ? "Adults Watch now" : "Minors are not available" }</button>
+      </>
+      
+    );
+  };
 
 export default App;
---
+++ src/Practice.jsx
@@ -0,0 +1,9 @@
+
+const Practice = () => {
+    const interview = "Muhammad Hassan you give a Interview"
+    return(
+        <h2>{interview}</h2>
+    )
+}
+
+export default Practice;
