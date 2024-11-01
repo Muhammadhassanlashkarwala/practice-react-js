5ef85b771faf7f08520adac290290a5f282c11d6
Muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2024-11-01 05:00:36 +0500

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import {  Header, AnimeArt } from "./components/AnimeArt";
-import NetflixSeries from "./components/NetflixSeries";
-import { Footer } from "./components/Footer";
-
+import seriesData from '../src/api/seriesData.json'
 const App = () => {
   return(
     <>
-    <Header />
-    <NetflixSeries />
-    <AnimeArt />
-    <Footer />
-    </>
-  )
+   <ul> 
+    {seriesData.map((curElem) => {
+     return(
+     <li key={curElem.id}>
+ <div>
+     <img src={curElem.image_url} width="40%" height="40%" alt="vite.svg" />
+   </div>
+    <h1>Name: {curElem.name}</h1>
+    <h3>Rating: {curElem.rating}</h3>
+    <h3>Description: {curElem.description}</h3>
+    <h3>Cast: {curElem.cast}</h3>
+    <h3>Genre: {curElem.genre}</h3>
+ </li>
+ );
+    })};
+   </ul>
+    </>  
+  );
 };
 export default App;
 
