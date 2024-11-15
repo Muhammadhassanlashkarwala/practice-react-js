5bf8c1d6a6275dbea249f5361b773cfc48298137
Muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2024-11-15 05:02:00 +0500

    push

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -9,9 +9,11 @@ export const SeriesCard = ({ curElem, key }) => {
     margin:"1.5rem 0rem",
     border:"none",
     fontSize:"rem",
+    cursor:"pointer",
     color:"white",
-    background:"#0062ff",
+    background: `${curElem.rating >= 8.5 ? "#7dcea0" : "#f7dc6f"}`
  }
+ const colorSetting = curElem.rating >= 8 ? "super_hit" : "average";         
     return(
         <li className="card" key={key}>
     <div>
@@ -19,13 +21,18 @@ export const SeriesCard = ({ curElem, key }) => {
       </div>
       <div className="card-content">
        <h1>Name: {curElem.name}</h1>
-       <h3 style={{margin:"1.5rem 0"}}>Rating: {curElem.rating}</h3>
+       <h3 style={{margin:"1.5rem 0"}}>
+        Rating: 
+       <span className={`rating ${colorSetting}`}>{curElem.rating}</span></h3>
        <p>Summary: {curElem.summary}</p>
        <p>Cast: {curElem.cast}</p>
        <p>Genre: {curElem.genre}</p>
       <button
       style={btn_style}
-      >{curElem.button}</button>
+      ><span className={`rating ${colorSetting}`}>
+        {curElem.button}
+      </span>
+      </button>
        </div>
     </li>
 
