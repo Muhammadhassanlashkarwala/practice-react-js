31804549e804ddef377645212703747228cade55
Muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2024-11-25 06:11:48 +0500

    push

--- src/EventProps.jsx
+++ src/EventProps.jsx
@@ -18,9 +18,9 @@ const WelcomeUser = (props) => {
     }
   return(
     <div className="text-center mt-20">
-    <button className="b-2 m-4  mt-20 px-2 py-8 text-4xl font-bold bg-purple-300 text-white" onClick={props.ClickButton}>Click</button> <br />
+    <button className="b-2 m-3  mt-20 px-2 py-8 text-4xl font-bold bg-purple-300 text-white" onClick={props.ClickButton}>Click</button> <br />
     <button className="b-2 m-20 px-2 py-8 text-4xl font-bold bg-pink-300 text-white" onMouseEnter={props.MouseEnter}>Hover me</button> <br />
-    <button className="b-2 m-4 px-2 py-8 text-4xl font-bold bg-cyan-300 text-white"  onClick={HandleGreeting}>Greeting</button>
+    <button className="b-2 m-3 px-2 py-8 text-4xl font-bold bg-cyan-300 text-white"  onClick={HandleGreeting}>Greeting</button>
     </div>
   )
 }
